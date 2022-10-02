555d7da9a351407bf1d40a094496c52cea97968f
erin <prisbreyerik@gmail.com>
2022-10-02 15:25:02 -0400

    done lol

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,32 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
+    <div>
+       <nav>
+         <a href="#home">this might be a link might not be!</a>
+       </nav>
+       <Home />
+       <About />
+    </div>
   );
 }
 
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h1>I should have been a messed up child star</h1>
     </div>
   );
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h1>i think amanda bynes is amazing</h1>
+    </div>
+  )
+}
 
 
 function App() {
